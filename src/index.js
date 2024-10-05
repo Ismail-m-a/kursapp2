6465e3bc70bec6c1ce1c074bd10e568a69d05e09
ismail-m-a <ismaaciilmuxamadaadam@gmail.com>
2024-10-05 15:50:42 +0200

    mm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Login from './components/Login/Login';
 import { ProductStore } from "./components/products";
-import { Register } from "./components/Register";
+import Register from "./components/Register/Register";
 import { CartContainer } from "./components/Productcart";
 import { Chat } from './components/Chat';
 import { HomePage } from './components/Homepage';
